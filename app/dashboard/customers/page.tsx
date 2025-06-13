d8cc85dd833092361b173c043d2cf89c6aacddf0
Yana Olik <yanaolik94@gmail.com>
2025-06-13 14:24:54 +0300

    fetch cutomers

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,31 @@
+import Pagination from '@/app/ui/invoices/pagination';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Metadata } from 'next';
+
+import Table from '@/app/ui/customers/table';
+import { Suspense } from 'react';
+import { fetchFilteredCustomers } from '@/app/lib/data';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <div className='w-full'>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table customers={customers} />
+      </Suspense>
+    </div>
+  );
 }
